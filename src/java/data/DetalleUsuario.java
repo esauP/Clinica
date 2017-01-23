54881267fdb42fdd638bfffadb871d4bd727c17c
esaup <esausalesianas@gmail.com>
2017-01-23 18:41:49 +0100

    añadida carga de tabla de prueba en log.jsp

--- src/java/data/DetalleUsuario.java
+++ src/java/data/DetalleUsuario.java
@@ -8,6 +8,8 @@ package data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import pojo.Usuario;
 
 /**
@@ -33,7 +35,7 @@ public class DetalleUsuario extends ConexionDB implements IUsuario {
 
         ps.setString(1, user);
         ps.setString(2, pass);
-        
+
         ResultSet rs = ps.executeQuery();
 
         if (rs.next()) {
@@ -44,6 +46,47 @@ public class DetalleUsuario extends ConexionDB implements IUsuario {
         this.desconectar();
 
         return us;
+    }
+
+    public List AllUsuarios() {
+
+        PreparedStatement stmt = null;
+        List searchResults = new LinkedList();
+        ResultSet result = null;
+
+        try {
+
+            String sql = "SELECT * FROM usuario";
+            stmt = this.getConexion().prepareStatement(sql);
+            stmt.executeQuery();
+            result = stmt.executeQuery();
+
+            while (result.next()) {
+                Usuario temp = new Usuario();
+                temp.setUsuario(result.getString("user"));
+                temp.setPass(result.getString("password"));
+                temp.setObservaciones(result.getString("observaciones"));
+
+                searchResults.add(temp);
+            }
+        } catch (SQLException e) {
+            System.out.println("Message: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("ErrorCode: " + e.getErrorCode());
+        } finally {
+
+            try {
+                if (result != null) {
+                    result.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+            }
+
+        }
+        return searchResults;
     }
 
 }
--- web/log.jsp
+++ web/log.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" import="java.util.*;" %>
+<%@ page language="java" import="java.util.*, data.*, pojo.*" %>
 <!DOCTYPE HTML>
 <hmtl>
     <body>
@@ -13,7 +13,37 @@
         
         <b> <h1>Usuario</h1> <h1> <%=session.getAttribute("observaciones")%> </h1></b>
 
-        
+         <div id="content">
+            <table summary ="Listado de Profesores"  cellSpacing="1" cellPadding="8" width="770" align="center" border="0">
+                <tr valign ="middle" align="center">
+                  
+                    <%
+                        DetalleUsuario det = new DetalleUsuario();
+                        List<Usuario> usuarios = det.AllUsuarios();
+                    %>
+                    <td id = "colchones" colspan="8"><b>Usuarios registrados</b></td> 
+                </tr>
+                <tr valign=" middle" align="center" id="cabecera">
+                    <td>Nombre</td><td>Contraseña</td><td>Observaciones</td><td>Seleccionar</td>
+                </tr>
+
+                <%  for (Iterator iter = usuarios.iterator(); iter.hasNext();) {
+                    Usuario u = (Usuario) iter.next();
+                %>
+                <tr align="center" id="Usuarios"> 
+                    <td><%=u.getUsuario()%></td>
+                    <td> <%=u.getPass()%> </td>
+                    <td> <%=u.getObservaciones()%> </td>
+                    <td> <a href="FrontController?res=tutorias.jsp?pid=<%=u.getUsuario()%>&name=<%=u.getPass()%>"><img src='img/seleccionar.gif'> </a></td>		 				
+                </tr>
+                <%
+                        }
+                    
+                %>
+
+            </table>
+
+        </div>
     </body>
 </hmtl>
   
